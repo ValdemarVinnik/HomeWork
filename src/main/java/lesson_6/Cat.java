9791f5012a477a8c8cc269e3bb0e1583f1c7b1ea
vladimir.vinnik <vladimir.vinnik.email@gmail.com>
2022-04-28 23:07:55 +0300

    home work6 improved

--- src/main/java/lesson_6/Cat.java
+++ src/main/java/lesson_6/Cat.java
@@ -7,4 +7,9 @@ public class Cat extends Animal {
         this.runningRestriction = 200;
         this.swimmingRestriction = 0;
     }
+
+    @Override
+    public void swim(int distance) {
+        System.out.println(name+" Bul-bul...");
+    }
 }
