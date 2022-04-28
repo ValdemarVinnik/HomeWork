b9f4787afc37322082d40f196137ec8038c40ba9
vladimir.vinnik <vladimir.vinnik.email@gmail.com>
2022-04-28 23:03:25 +0300

    home work6 finish

--- src/main/java/lesson_6/Cat.java
+++ src/main/java/lesson_6/Cat.java
@@ -1,4 +1,10 @@
 package lesson_6;
 
 public class Cat extends Animal {
+
+    public Cat(String name) {
+        super(name);
+        this.runningRestriction = 200;
+        this.swimmingRestriction = 0;
+    }
 }
--- src/main/java/lesson_6/Dog.java
+++ src/main/java/lesson_6/Dog.java
@@ -1,4 +1,10 @@
 package lesson_6;
 
 public class Dog extends Animal {
+
+    public Dog(String name) {
+        super(name);
+        this.runningRestriction = 500;
+        this.swimmingRestriction = 10;
+    }
 }
