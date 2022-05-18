288e124107d95693313c0269b3e364989c5f44a9
vladimir.vinnik <vladimir.vinnik.email@gmail.com>
2022-05-18 23:01:11 +0300

    home work8 something do (Listener)

--- src/main/java/lesson_8/Transfer.java
+++ src/main/java/lesson_8/Transfer.java
@@ -2,33 +2,63 @@ package lesson_8;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Transfer extends JFrame {
+    private JLabel firstDisplay;
     String alphabet = "йцукенгшщзхъфывапролджэячсмитьбю.,";
 
+   ActionListener letterListener = new ActionListener(){
+
+       @Override
+       public void actionPerformed(ActionEvent actionEvent) {
+           JButton button = (JButton) actionEvent.getSource();
+           String message = firstDisplay.getText();
+            message += button.getText();
+            firstDisplay.setText(message);
+       }
+   };
 
     public Transfer() {
         setTitle("Рация шпиЁна");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setBounds(300, 300, 300, 300);
 
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new GridLayout(2,1));
+        firstDisplay = new JLabel("->");
+        firstDisplay.setHorizontalAlignment(SwingConstants.LEFT);
+        firstDisplay.setFont(new Font("Arial", Font.BOLD, 20));
 
-        JTextField field = new JTextField();
+        JLabel secondDisplay = new JLabel("<-");
+        secondDisplay.setHorizontalAlignment(SwingConstants.LEFT);
+        secondDisplay.setFont(new Font("Arial", Font.BOLD, 20));
+       jPanel.add(firstDisplay);
+        jPanel.add(secondDisplay);
 
-        add(field, BorderLayout.NORTH);
+        add(jPanel,BorderLayout.NORTH);
+        add(setLetters(), BorderLayout.CENTER);
 
-        setLayout(new FlowLayout());
 
-       // final JPanel
-        for (String each: alphabet.split("")) {
-            add(new JButton(each));
-
-        }
         setVisible(true);
     }
 
-    public static void main(String[] args) {
+    private JPanel setLetters(){
 
+        final JPanel lettersPanel = new JPanel();
+        lettersPanel.setLayout(new FlowLayout());
+        for (String each: alphabet.split("")) {
+            JButton button = new JButton(each);
+            button.addActionListener(letterListener);
+            lettersPanel.add(button);
+        }
+        lettersPanel.add(new Button("space"));
+        return  lettersPanel;
+    }
+
+    public static void main(String[] args) {
+new Transfer();
 
     }
 }
