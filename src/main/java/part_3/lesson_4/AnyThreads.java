e74d33f8aaefbde3bf382822a954b2a5aceeb726
vladimir.vinnik <vladimir.vinnik.email@gmail.com>
2022-08-26 21:44:22 +0300

    finish all

--- src/main/java/part_3/lesson_4/AnyThreads.java
+++ src/main/java/part_3/lesson_4/AnyThreads.java
@@ -1,20 +1,19 @@
 package part_3.lesson_4;
 
 public class AnyThreads {
-    private volatile String pattern = "ABC";
+    private final int REPEAT = 5;
     private volatile String currentLetter = "C";
     private volatile String lastLetter = "B";
 
     private final Object monitor = new Object();
 
-    private void printLetter(Boolean printIsPossible, String letter) {
+    private void printLetter(String letter) {
 
         synchronized (monitor) {
 
-            while ((!lastLetter.equals(letter )||currentLetter.equals(letter))) {
+            while ((lastLetter.equals(letter) || currentLetter.equals(letter))) {
                 try {
                     monitor.wait();
-
                 } catch (InterruptedException | IllegalMonitorStateException e) {
                     e.printStackTrace();
                 }
@@ -22,26 +21,26 @@ public class AnyThreads {
             System.out.print(letter);
             lastLetter = currentLetter;
             currentLetter = letter;
-            monitor.notify();
+            monitor.notifyAll();
         }
     }
 
     public void chainOfThreads() {
         new Thread(() -> {
-            for (int i = 1; i <= 5; i++) {
-                printLetter((currentLetter.equals("C")), "A");
+            for (int i = 1; i <= REPEAT; i++) {
+                printLetter("A");
             }
         }).start();
 
         new Thread(() -> {
-            for (int i = 1; i <= 5; i++) {
-                printLetter((currentLetter.equals("A")), "B");
+            for (int i = 1; i <= REPEAT; i++) {
+                printLetter("B");
             }
         }).start();
 
         new Thread(() -> {
-            for (int i = 1; i <= 5; i++) {
-                printLetter((currentLetter.equals("B")), "C");
+            for (int i = 1; i <= REPEAT; i++) {
+                printLetter("C");
             }
 
         }).start();
